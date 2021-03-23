cc792f0958059c26057cc4e4c4c2cfe5ae113322
autentificaMAMR41 <81258076+autentificaMAMR41@users.noreply.github.com>
2021-03-23 13:15:36 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,5 +8,5 @@ apiKey: "AIzaSyAxGzJY7UB93g5k8YSwzOqy1RAKwhmdIqc",
     storageBucket: "autentificamamr41.appspot.com",
     messagingSenderId: "384052832935",
     appId: "1:384052832935:web:dd217d142c2bb00675c982",
-    measurementId: "G-XRCH9CMDDB"63PHHW"
+    measurementId: "G-XRCH9CMDDB"
     });
