54d616e1098d1bf77e7aceb86455cb49cdee74d2
autentificaMAMR41 <81258076+autentificaMAMR41@users.noreply.github.com>
2021-03-23 13:09:47 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyBKXKRaQCWdfiSCZR39wunK0QApZMhgeqE",
-    authDomain: "pauten43.firebaseapp.com",
-    projectId: "pauten43",
-    storageBucket: "pauten43.appspot.com",
-    messagingSenderId: "167024006136",
-    appId: "1:167024006136:web:2766577637b6ab37c43c0d",
-    measurementId: "G-S4KD63PHHW"
+apiKey: "AIzaSyAxGzJY7UB93g5k8YSwzOqy1RAKwhmdIqc",
+    authDomain: "autentificamamr41.firebaseapp.com",
+    projectId: "autentificamamr41",
+    storageBucket: "autentificamamr41.appspot.com",
+    messagingSenderId: "384052832935",
+    appId: "1:384052832935:web:dd217d142c2bb00675c982",
+    measurementId: "G-XRCH9CMDDB"63PHHW"
     });
